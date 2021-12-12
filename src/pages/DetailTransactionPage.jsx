95d9e3e5bca7df584169e66ab551c689797fae5e
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:04:22 +0700

    feat(page): add detail transaction page

---
+++ src/pages/DetailTransactionPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import DetailTransaction from '../components/DetailTransaction';
+import Footer from '../components/Footer';
+
+export default function DetailTransactionPage() {
+  return (
+    <>
+      <Header />
+      <DetailTransaction />
+      <Footer />
+    </>
+  );
+}
