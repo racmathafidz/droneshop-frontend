ea917914d65892f2a1781a9e6c76b3b5a4b74ce3
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 16:48:08 +0700

    feat(components): add all products component

---
+++ src/components/AllProducts.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { products } from '../json/products.json';
+
+export default function NewProducts() {
+  return (
+    <section className="all-products">
+      <div className="all-products-title">
+        <h1>All Products</h1>
+        <p>
+          All innovation with best quality.
+          <br />
+          Make every moment flying operation become unforgettable.
+        </p>
+      </div>
+      <div className="all-products-list">
+        {
+          products
+            .map((items) => (
+              <div className="products-list" key={items.name}>
+                <img src={items.images} alt={items.name} />
+                <h1 className="products-name">{items.name}</h1>
+                <h1 className="products-price">{items.price}</h1>
+                <p className="products-description">{items.description}</p>
+                <div className="products-button">
+                  <Link to="/product">Go Now</Link>
+                </div>
+              </div>
+            ))
+        }
+      </div>
+    </section>
+  );
+}
