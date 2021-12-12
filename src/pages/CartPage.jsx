db2ac55a5a275f741201ab952a015f21432be607
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:01:15 +0700

    feat(page): add cart page

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Cart from '../components/Cart';
+import Footer from '../components/Footer';
+
+export default function CartPage() {
+  return (
+    <>
+      <Header />
+      <Cart />
+      <Footer />
+    </>
+  );
+}
