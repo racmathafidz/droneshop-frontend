ab983bb6a7faaa8e0d4e61ed7bed13b8a0df1b8b
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:08:45 +0700

    feat(page): add  products page

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import AllProducts from '../components/AllProducts';
+import Footer from '../components/Footer';
+
+export default function ProductsPage() {
+  return (
+    <>
+      <Header />
+      <AllProducts />
+      <Footer />
+    </>
+  );
+}
