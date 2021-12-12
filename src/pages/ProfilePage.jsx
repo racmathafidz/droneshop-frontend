5a3ec7904c485edc8caae645d7272214c9447cc4
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:08:59 +0700

    feat(page): add profile page

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Profile from '../components/Profile';
+import Footer from '../components/Footer';
+
+export default function ProfilePage() {
+  return (
+    <>
+      <Header />
+      <Profile />
+      <Footer />
+    </>
+  );
+}
