6d563117923135f4806aa9dd998c8d92cb952bc3
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:04:38 +0700

    feat(page): add gallery page

---
+++ src/pages/GalleryPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Gallery from '../components/Gallery';
+import Footer from '../components/Footer';
+
+export default function GalleryPage() {
+  return (
+    <>
+      <Header />
+      <Gallery />
+      <Footer />
+    </>
+  );
+}
