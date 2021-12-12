4c7499420625001fe429fa74e49769da280bbd96
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:08:27 +0700

    feat(page): add not found page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import NotFound from '../components/NotFound';
+import Footer from '../components/Footer';
+
+export default function DetailProductPage() {
+  return (
+    <>
+      <Header />
+      <NotFound />
+      <Footer />
+    </>
+  );
+}
