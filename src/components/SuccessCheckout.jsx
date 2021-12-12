0b2be29234f09c5cf606b239f3e30fea3dc78eb6
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 16:57:26 +0700

    feat(components): add success checkout component

---
+++ src/components/SuccessCheckout.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import PaymentImage from '../assets/images/Payment/payment.png';
+
+export default function SuccessCheckout() {
+  return (
+    <section className="success-checkout">
+      <div className="success-checkout-container">
+        <div className="success-checkout-container-image">
+          <img src={PaymentImage} alt="Waiting for payment" />
+        </div>
+        <h1>Waiting for payment</h1>
+        <div className="payment-container">
+          <div className="payment-method">
+            <p>Payment method</p>
+            <p className="payment-method-info">Jaog Bank</p>
+          </div>
+          <div className="payment-method">
+            <p>Payment code</p>
+            <p className="payment-method-info">121231234</p>
+          </div>
+          <div className="payment-method">
+            <p>Total payment</p>
+            <p className="payment-method-info">Rp190.000.000</p>
+          </div>
+        </div>
+        <div className="check-payment-status">
+          <Link to="/payment">Payment status</Link>
+        </div>
+      </div>
+    </section>
+  );
+}
