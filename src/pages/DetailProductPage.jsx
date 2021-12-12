d74c4ef32c5aa4f057d7f172d70794da14e69a64
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:03:52 +0700

    feat(page): add detail product page

---
+++ src/pages/DetailProductPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import DetailProduct from '../components/DetailProduct';
+import Footer from '../components/Footer';
+
+export default function DetailProductPage() {
+  return (
+    <>
+      <Header />
+      <DetailProduct />
+      <Footer />
+    </>
+  );
+}
