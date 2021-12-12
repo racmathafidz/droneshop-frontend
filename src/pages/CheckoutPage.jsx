c99253fff79538c0386305fe97eba7e9fcde91d2
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:03:34 +0700

    feat(page): add checkout page

---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Checkout from '../components/Checkout';
+import Footer from '../components/Footer';
+
+export default function CheckoutPage() {
+  return (
+    <>
+      <Header />
+      <Checkout />
+      <Footer />
+    </>
+  );
+}
