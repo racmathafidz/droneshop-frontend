ed1d057723f47fe5cd616ab1d294a10a9a6176d6
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:09:46 +0700

    feat(page): add success checkout page

---
+++ src/pages/SuccessCheckoutPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import SuccessCheckout from '../components/SuccessCheckout';
+import Footer from '../components/Footer';
+
+export default function SuccessCheckoutPage() {
+  return (
+    <>
+      <Header />
+      <SuccessCheckout />
+      <Footer />
+    </>
+  );
+}
