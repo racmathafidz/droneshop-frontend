4f2d6157a9d0be23be9d7221f36d69c7dc8a5dea
racmathafidz <rac.hafidz@gmail.com>
2021-12-12 17:09:31 +0700

    feat(page): add signup page

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { Link } from 'react-router-dom';
+
+import GoogleButton from '../components/GoogleButton';
+import BrandLogo from '../assets/images/Header/Logo.svg';
+import HeroImage from '../assets/images/Header/hero-image.png';
+
+export default function SignUpPage() {
+  const googleSignupHandler = (response) => {
+    console.log(response);
+  };
+
+  return (
+    <section className="auth">
+      <img src={HeroImage} alt="Drone" width="832px" className="auth-image" />
+      <div className="signup-form">
+        <Link to="/" className="auth-logo">
+          <img src={BrandLogo} alt="Drone Shop ID" />
+          <p>DroneShopID</p>
+        </Link>
+        <h1 className="auth-title">Create New Account</h1>
+        <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          render={(renderProps) => (
+            <GoogleButton onClick={renderProps.onClick} />
+          )}
+          buttonText="Continue with Google"
+          onSuccess={googleSignupHandler}
+          onFailure={googleSignupHandler}
+          cookiePolicy="single_host_origin"
+        />
+        <p className="auth-or-text">
+          <span className="auth-or-text-span">
+            OR
+          </span>
+        </p>
+        <form action="" className="auth-input-form">
+          <label htmlFor="name" className="auth-label">
+            Full Name
+            <input
+              type="text"
+              name="fullName"
+              id="fullName"
+              placeholder="Full Name"
+              className="auth-input"
+            />
+          </label>
+          <label htmlFor="email" className="auth-label">
+            Email Address
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Email Address"
+              className="auth-input"
+            />
+          </label>
+          <label htmlFor="password" className="auth-label">
+            Password
+            <input
+              type="text"
+              name="password"
+              id="password"
+              placeholder="Password"
+              className="auth-input"
+            />
+          </label>
+          <input type="submit" value="Sign Up" className="auth-button" />
+        </form>
+        <p className="auth-change">
+          Already have an account?
+          {' '}
+          <Link to="/signin">Sign In</Link>
+        </p>
+      </div>
+    </section>
+  );
+}
