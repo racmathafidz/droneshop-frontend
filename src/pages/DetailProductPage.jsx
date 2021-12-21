d2822743de1b17f837afa6dd9f84dcdc5a8befd9
racmathafidz <rac.hafidz@gmail.com>
2021-12-21 21:48:56 +0700

    feat(fetch API and redux): fetching API and then save to redux state

--- src/pages/DetailProductPage.jsx
+++ src/pages/DetailProductPage.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
+import LoadingPage from './LoadingPage';
 import Header from '../components/Header';
 import DetailProduct from '../components/DetailProduct';
 import Footer from '../components/Footer';
 
 export default function DetailProductPage() {
+  const { id } = useParams();
+  const [productData, setProductData] = useState();
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/product/${id}`)
+      .then((response) => {
+        setProductData(response.data);
+      });
+  }, []);
+
+  if (productData) {
+    return (
+      <>
+        <Header />
+        <DetailProduct ProductData={productData} />
+        <Footer />
+      </>
+    );
+  }
+
   return (
-    <>
-      <Header />
-      <DetailProduct />
-      <Footer />
-    </>
+    <LoadingPage />
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import axios from 'axios';
 
+import LoadingPage from './LoadingPage';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Content from '../components/Content';
@@ -8,18 +12,38 @@ import Video from '../components/Video';
 import NewProducts from '../components/NewProducts';
 import Testimonial from '../components/Testimonial';
 import Footer from '../components/Footer';
+import { ActionCreators } from '../redux/actions';
 
 export default function LandingPage() {
+  const dispatch = useDispatch();
+  const { AddProductsDataAction } = bindActionCreators(ActionCreators, dispatch);
+  const State = useSelector((state) => state);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/product`)
+      .then((response) => {
+        AddProductsDataAction({
+          ProductsData: response.data,
+        });
+      });
+  }, []);
+
+  if (State.ProductsData.ProductsData) {
+    return (
+      <>
+        <Header />
+        <Hero />
+        <Content />
+        <Benefit />
+        <Video />
+        <NewProducts ProductsData={State.ProductsData.ProductsData} />
+        <Testimonial />
+        <Footer />
+      </>
+    );
+  }
+
   return (
-    <>
-      <Header />
-      <Hero />
-      <Content />
-      <Benefit />
-      <Video />
-      <NewProducts />
-      <Testimonial />
-      <Footer />
-    </>
+    <LoadingPage />
   );
 }
