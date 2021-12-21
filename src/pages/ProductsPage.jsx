c1ee055a018e0ff91f1437ef68a4df040961dc8e
racmathafidz <rac.hafidz@gmail.com>
2021-12-21 21:49:35 +0700

    feat(redux state): using redux state for real data

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
+import LoadingPage from './LoadingPage';
 import Header from '../components/Header';
 import AllProducts from '../components/AllProducts';
 import Footer from '../components/Footer';
 
 export default function ProductsPage() {
+  const State = useSelector((state) => state);
+
+  if (State.ProductsData.ProductsData) {
+    return (
+      <>
+        <Header />
+        <AllProducts ProductsData={State.ProductsData.ProductsData} />
+        <Footer />
+      </>
+    );
+  }
+
   return (
-    <>
-      <Header />
-      <AllProducts />
-      <Footer />
-    </>
+    <LoadingPage />
   );
 }
