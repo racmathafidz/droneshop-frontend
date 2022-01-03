9ed7a0be291b24bf0392e76036d2ce331e76b26f
racmathafidz <rac.hafidz@gmail.com>
2022-01-03 11:25:30 +0700

    [FEAT] Add button onClick function

--- src/components/GoogleButton.jsx
+++ src/components/GoogleButton.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 import GoogleLogo from '../assets/images/Google/google.png';
 
-export default function GoogleButton() {
+export default function GoogleButton(props) {
+  const { onClick } = props;
+
   return (
-    <div className="google-button">
+    <button type="button" onClick={() => onClick()} className="google-button">
       <div className="google-button-logo">
         <img src={GoogleLogo} alt="Google" />
       </div>
       <div className="google-button-text">
         <p>Continue with Google</p>
       </div>
-    </div>
+    </button>
   );
 }
